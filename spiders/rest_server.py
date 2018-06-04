0f402cd22641cb21422efa5dcc57f343070ce491
tutturu <n.ryaskin@gmail.com>
2018-06-04 16:05:36 -0400

    Change 2 to 3 python version

--- spiders/rest_server.py
+++ spiders/rest_server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-import sys, os, re, shutil, json, urllib, urllib2, BaseHTTPServer, operator
-from words import words_by_url
-from links import urls_by_word
+import sys, os, re, shutil, json, urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse, http.server, operator
+from .words import words_by_url
+from .links import urls_by_word
+from functools import reduce
+import imp
 # Fix issues with decoding HTTP responses
-reload(sys)
+imp.reload(sys)
 sys.setdefaultencoding('utf8')
 
 here = os.path.dirname(os.path.realpath(__file__))
@@ -14,20 +16,20 @@ records = {}
 def get_params(url):
     params = url.split("?")[1]
     params = params.split('=')
-    pairs = zip(params[0::2], params[1::2])
+    pairs = list(zip(params[0::2], params[1::2]))
     answer = dict((k,v) for k,v in pairs)
     return answer
 
 def decode_values(path):
     params = get_params(path)
-    return urllib.unquote(params['values']).decode('utf8')
+    return urllib.parse.unquote(params['values']).decode('utf8')
 
 def by_func(func, k, path):
     params = get_params(path)
-    values = urllib.unquote(params['values']).decode('utf8').split(' ')
+    values = urllib.parse.unquote(params['values']).decode('utf8').split(' ')
     if not values:
         return {k: []}
-    result = {k: list(set(reduce(operator.concat, map(lambda url: func(url), values))))}
+    result = {k: list(set(reduce(operator.concat, [func(url) for url in values])))}
     return result
 
 def get_words(handler):
@@ -39,30 +41,30 @@ def get_urls(handler):
     return res
 
 def set_record(handler):
-    key = urllib.unquote(handler.path[8:])
+    key = urllib.parse.unquote(handler.path[8:])
     payload = handler.get_payload()
     records[key] = payload
     return records[key]
 
 def delete_record(handler):
-    key = urllib.unquote(handler.path[8:])
+    key = urllib.parse.unquote(handler.path[8:])
     del records[key]
     return True # anything except None shows success
 
 def rest_call_json(url, payload=None, with_payload_method='PUT'):
     'REST call with JSON decoding of the response and JSON payloads'
     if payload:
-        if not isinstance(payload, basestring):
+        if not isinstance(payload, str):
             payload = json.dumps(payload)
         # PUT or POST
-        response = urllib2.urlopen(MethodRequest(url, payload, {'Content-Type': 'application/json'}, method=with_payload_method))
+        response = urllib.request.urlopen(MethodRequest(url, payload, {'Content-Type': 'application/json'}, method=with_payload_method))
     else:
         # GET
-        response = urllib2.urlopen(url)
+        response = urllib.request.urlopen(url)
     response = response.read().decode()
     return json.loads(response)
 
-class MethodRequest(urllib2.Request):
+class MethodRequest(urllib.request.Request):
     'See: https://gist.github.com/logic/2715756'
     def __init__(self, *args, **kwargs):
         if 'method' in kwargs:
@@ -70,12 +72,12 @@ class MethodRequest(urllib2.Request):
             del kwargs['method']
         else:
             self._method = None
-        return urllib2.Request.__init__(self, *args, **kwargs)
+        return urllib.request.Request.__init__(self, *args, **kwargs)
 
     def get_method(self, *args, **kwargs):
-        return self._method if self._method is not None else urllib2.Request.get_method(self, *args, **kwargs)
+        return self._method if self._method is not None else urllib.request.Request.get_method(self, *args, **kwargs)
 
-class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class RESTRequestHandler(http.server.BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         self.routes = {
             r'^/index.html$': {'file': 'index.html', 'media_type': 'text/html'},
@@ -85,7 +87,7 @@ class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             r'^/words.*': {'GET': get_words, 'media_type': 'application/json'},
             r'^/urls.*': {'GET': get_urls, 'media_type': 'application/json'}}
         
-        return BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+        return http.server.BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
     
     def do_HEAD(self):
         self.handle_method('HEAD')
@@ -162,20 +164,20 @@ class RESTRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                     
     
     def get_route(self):
-        for path, route in self.routes.iteritems():
+        for path, route in self.routes.items():
             if re.match(path, self.path):
                 return route
         return None
 
 def rest_server(port):
     'Starts the REST server'
-    http_server = BaseHTTPServer.HTTPServer(('', port), RESTRequestHandler)
-    print 'Starting HTTP server at port %d' % port
+    http_server = http.server.HTTPServer(('', port), RESTRequestHandler)
+    print('Starting HTTP server at port %d' % port)
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
         pass
-    print 'Stopping HTTP server'
+    print('Stopping HTTP server')
     http_server.server_close()
 
 def main(argv):
